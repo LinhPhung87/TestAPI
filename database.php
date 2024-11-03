487c21f2f3e2bdfcf0e50764e0508cddf01255f1
LinhPhung87 <pm.linh.87@gmail.com>
2024-11-03 16:58:01 +0700

    Delete database.php

--- database.php
+++
@@ -1,6 +0,0 @@
-<?php
-    ob_start();
-    session_start();
-    $con=mysqli_connect('sql106.infinityfree.com','if0_37497458','INruINR4CcA','if0_37497458_leavemanagementsystem');
-    mysqli_set_charset($con, 'UTF8');
-?>
