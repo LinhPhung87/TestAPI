0f35509dac59a3f844a8fcc2b3342543376fdd77
LinhPhung87 <pm.linh.87@gmail.com>
2024-11-03 10:55:22 +0700

    database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php
     ob_start();
     session_start();
-    $con=mysqli_connect('localhost','root','','leavemanagementsystem');
+    $con=mysqli_connect('sql106.infinityfree.com','if0_37497458','INruINR4CcA','if0_37497458_leavemanagementsystem');
     mysqli_set_charset($con, 'UTF8');
 ?>
